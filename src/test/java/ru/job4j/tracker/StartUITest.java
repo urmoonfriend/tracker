79f52ea5dcb92ee3918b30640d31bb00b939561e
staskorobeynikov <stas.korobeinikov@mail.ru>
2021-04-03 20:52:19 +0300

    1.1. Тесты на StartUI. [#223313]

---
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,31 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class StartUITest {
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"10", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "Menu:" + ln
+                        + "0. Exit Program" + ln
+                        + "Wrong input, you can select: 0 .. 0" + ln
+                        + "Menu:" + ln
+                        + "0. Exit Program" + ln
+                        + "=== Exit Program ===" + ln
+                )
+        );
+    }
+}
