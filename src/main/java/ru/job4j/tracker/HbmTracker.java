c8b3c772fbb1386f1326d1f65e66769fba1816a8
Aidos <aidos@paygate.kz>
2023-09-25 09:04:39 +0600

    add configs and HQLUsage

---
+++ src/main/java/ru/job4j/tracker/HbmTracker.java
@@ -0,0 +1,96 @@
+package ru.job4j.tracker;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.List;
+
+public class HbmTracker implements Store, AutoCloseable {
+    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure().build();
+    private final SessionFactory sf = new MetadataSources(registry)
+            .buildMetadata().buildSessionFactory();
+
+    @Override
+    public Item add(Item item) {
+        Session session = sf.openSession();
+        Item itemToReturn = null;
+        try {
+            session.beginTransaction();
+            session.save(item);
+            session.getTransaction().commit();
+            itemToReturn = item;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return itemToReturn;
+    }
+
+    @Override
+    public boolean replace(Integer id, Item item) {
+        Session session = sf.openSession();
+        boolean result = false;
+        try {
+            session.beginTransaction();
+            session.createQuery("UPDATE Item SET name = :fName WHERE id = :fId").setParameter("fName", item.getName())
+                    .setParameter("fId", id).executeUpdate();
+            session.getTransaction().commit();
+            result = true;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return result;
+    }
+
+    @Override
+    public void delete(Integer id) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery("DELETE Item WHERE id = :fId").setParameter("fId", id).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+    }
+
+    @Override
+    public List<Item> findAll() {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        List<Item> result = session.createQuery("from Item", Item.class).list();
+        session.getTransaction().commit();
+        session.close();
+        return result;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        List<Item> result = session.createQuery("from Item WHERE name = :fName", Item.class)
+                .setParameter("fName", key).list();
+        session.getTransaction().commit();
+        session.close();
+        return result;
+    }
+
+    @Override
+    public Item findById(Integer id) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        Item result = session.createQuery("from Item WHERE id = :fId", Item.class)
+                .setParameter("fId", id).uniqueResult();
+        session.getTransaction().commit();
+        session.close();
+        return result;
+    }
+
+    @Override
+    public void close() {
+        StandardServiceRegistryBuilder.destroy(registry);
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/Store.java
@@ -0,0 +1,17 @@
+package ru.job4j.tracker;
+
+import java.util.List;
+
+public interface Store {
+    Item add(Item item);
+
+    boolean replace(Integer id, Item item);
+
+    void delete(Integer id);
+
+    List<Item> findAll();
+
+    List<Item> findByName(String key);
+
+    Item findById(Integer id);
+}
