7d15762198274eadcf6cb997fe338443cadb1325
Aidos <aidos@paygate.kz>
2023-09-25 09:04:39 +0600

    add configs and HQLUsage

---
+++ src/main/java/ru/job4j/tracker/HQLUsage.java
@@ -0,0 +1,63 @@
+package ru.job4j.tracker;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import javax.persistence.Query;
+
+public class HQLUsage {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        try (SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory()) {
+            Session session = sf.openSession();
+            findById(session, 2);
+            session.close();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+    public static void unique(Session session) {
+        Query query = session.createQuery("from Item as i where i.id = 3", Item.class);
+        System.out.println(((org.hibernate.query.Query<?>) query).uniqueResult());
+    }
+
+    public static void findById(Session session, int id) {
+        Query query = session.createQuery("from Item as i where i.id = :fId", Item.class);
+        query.setParameter("fId", id);
+        System.out.println(((org.hibernate.query.Query<?>) query).uniqueResult());
+    }
+
+    public static void update(Session session, int id) {
+        try {
+            session.beginTransaction();
+            session.createQuery("UPDATE Item SET name = :fName WHERE id = :fId").setParameter("fName", "new name").setParameter("fId", id).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+    }
+
+    public static void delete(Session session, int id) {
+        try {
+            session.beginTransaction();
+            session.createQuery("DELETE Item WHERE id = :fId").setParameter("fId", id).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+    }
+
+    public static void insert(Session session, Item item) {
+        try {
+            session.beginTransaction();
+            session.save(item);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+    }
+}
--- src/main/resources/db/scripts/001_ddl_create_initial_schema.sql
+++ src/main/resources/db/scripts/001_ddl_create_initial_schema.sql
@@ -1,5 +1,4 @@
-create table if not exists items
-(
+create table if not exists items (
     id serial primary key,
     name text,
     created timestamp
